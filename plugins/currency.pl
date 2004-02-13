6ac35743566c14ed77f0e99a1975329f15389632
kstange <kstange>
2004-02-13 20:02:56 +0000

    Infobot says it is licensed the same as Perl, which means this is GPL.

--- plugins/currency.pl
+++ plugins/currency.pl
@@ -2,6 +2,20 @@
 #
 # Mostly taken from exchange.pl from infobot 0.45.3 and 0.49_03
 # modified for SimBot by Pete Pearson
+#
+# This program is free software; you can redistribute it and/or modify
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package SimBot::plugin::currency;
 use HTTP::Request::Common qw(POST GET);
