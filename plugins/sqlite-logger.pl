0aed3815e76f89b843171b7d2e73aa9366808071
kstange <kstange>
2004-12-30 21:32:22 +0000

    16:30 < FourOhThree> topic should be there too, I think

--- plugins/sqlite-logger.pl
+++ plugins/sqlite-logger.pl
@@ -323,7 +323,7 @@ sub access_log {
             }
         
             while(my $cur_arg = shift(@args)) {
-                if($cur_arg =~ /(say|join|part|quit|nick|kick|notice|action)/i) {
+                if($cur_arg =~ /(say|join|part|quit|nick|kick|notice|action|topic)/i) {
                     my $cur_event = $1;
                     if($cur_event =~ /^(join|part|quit|kick)$/i) 
                         { $cur_event .= 'ed'; }
